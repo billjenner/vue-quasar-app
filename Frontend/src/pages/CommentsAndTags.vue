6efa54fd40cbb91b9149d5648cc64375e6097c6e
billjenner <bill.jenner@gmail.com>
2025-04-20 19:03:30 -0700

    Add Date Range Criteria

--- Frontend/src/pages/CommentsAndTags.vue
+++ Frontend/src/pages/CommentsAndTags.vue
@@ -17,6 +17,17 @@
     </div>
 
     <div class="row">
+      <div class="q-pa-md">
+        <q-date 
+          v-model="dateRange" 
+          mask="YYYY-MM-DD"
+          range 
+          @update:model-value="updateFilteredCommentsByDate" 
+        />
+      </div>
+    </div>
+
+    <div class="row">
       <div class="col-2 q-pa-md">
         <q-select
           filled
@@ -57,42 +68,45 @@
     </div>
 
     <div class="q-mt-xl">    <!-- Comments list -->
-    <q-list>
-      <q-item v-for="comment in filteredComments" :key="comment.id">
-        <q-item-section class="text-left">
-          <div>{{ comment.text }}</div>
-          <q-dialog v-model="showTagModalVisible[comment.id]">
-            <q-card>
-              <q-card-section>
-                <div>Add tags:</div>
-                <q-input
-                  v-model="newHashtags[comment.id]"
-                  outlined
-                  clearable
-                  placeholder="Enter a hashtag and press Enter"
-                  @keyup.enter="addHashtagToComment(comment.id)"
-                />
-              </q-card-section>
-              <q-card-actions align="right">
-                <q-btn flat label="Cancel" color="primary" v-close-popup />
-              </q-card-actions>
-            </q-card>
-          </q-dialog>
+      <q-list>
+        <q-item v-for="comment in filteredComments" :key="comment.id">
+          <q-item-section class="text-left">
+            <div>
+              {{ comment.text }}
+              <span class="text-caption text-grey-7"> ({{ comment.date }})</span>
+            </div>
+            <q-dialog v-model="showTagModalVisible[comment.id]">
+              <q-card>
+                <q-card-section>
+                  <div>Add tags:</div>
+                  <q-input
+                    v-model="newHashtags[comment.id]"
+                    outlined
+                    clearable
+                    placeholder="Enter a hashtag and press Enter"
+                    @keyup.enter="addHashtagToComment(comment.id)"
+                  />
+                </q-card-section>
+                <q-card-actions align="right">
+                  <q-btn flat label="Cancel" color="primary" v-close-popup />
+                </q-card-actions>
+              </q-card>
+            </q-dialog>
 
-          <div>
-            <q-btn @click="showTagModal(comment.id)" flat size="sm" class="col-6">Click to add tags...</q-btn>
-            <q-chip
-              v-for="tag in comment.hashtags"
-              :key="tag"
-              removable
-              @remove="removeHashtag(comment.id, tag)"
-            >
-              {{ tag }}
-            </q-chip>
-          </div>
-        </q-item-section>
-      </q-item>
-    </q-list>
+            <div>
+              <q-btn @click="showTagModal(comment.id)" flat size="sm" class="col-6">Click to add tags...</q-btn>
+              <q-chip
+                v-for="tag in comment.hashtags"
+                :key="tag"
+                removable
+                @remove="removeHashtag(comment.id, tag)"
+              >
+                {{ tag }}
+              </q-chip>
+            </div>
+          </q-item-section>
+        </q-item>
+      </q-list>
   </div>
 </div>
 </div>
@@ -116,13 +130,17 @@ export default {
         { label: 'Discussion', value: 'Discussion' },
       ],
       comments: [
-  { id: 1, text: 'This is a comment', hashtags: ['tag2', 'tag1'], category: 'General' },
-  { id: 2, text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate impedit eveniet deserunt, quam atque nisi deleniti esse perferendis commodi voluptas eaque autem quidem quibusdam, corrupti, ratione facilis fugiat molestias distinctio dolores aliquam. Asperiores maiores eligendi quas ducimus quam mollitia ad eius nisi. Repellat hic officiis ea exercitationem natus vero quos!', hashtags: ['tag3', 'tag4'], category: 'Feedback' },
-  { id: 3, text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur, pariatur! Repudiandae tenetur ea maxime voluptatem saepe commodi adipisci dignissimos molestias?', hashtags: ['tag11', 'tag4'], category: 'Suggestion' },
-  { id: 4, text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ', hashtags: ['tag5'], category: 'Question' },
-  { id: 5, text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus, reprehenderit cumque?', hashtags: ['tag6'], category: 'Idea' },
-  { id: 6, text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos quas, fuga neque voluptas et, a quam impedit obcaecati maiores repellat facere amet dolore tempora ad vel iusto fugiat temporibus laborum dicta praesentium ducimus corrupti reprehenderit animi voluptate? Optio ex quia accusamus labore eveniet nesciunt porro doloremque consequuntur sapiente, sint ducimus perspiciatis. Aspernatur vitae optio alias velit ea, atque corporis suscipit aliquam doloribus! Quos quae sit vel accusamus aliquam quam totam, magnam omnis id facere, dolorum atque asperiores nulla unde praesentium vitae repellat minus, voluptas mollitia ipsa? Quisquam officiis hic sint, corrupti nihil amet consequuntur, illum incidunt earum aperiam quibusdam modi rerum tenetur ullam alias aut voluptatibus ea, in exercitationem deserunt consectetur laboriosam, nihil iure error dolor fuga reiciendis enim! Ipsam, necessitatibus labore autem unde, obcaecati esse corrupti debitis exercitationem enim aspernatur aliquid. Sed fugiat officia expedita assumenda nisi neque quis eligendi repellat, architecto praesentium placeat eos eveniet! Quaerat temporibus sapiente aliquid distinctio ducimus sunt officiis harum, alias mollitia aliquam perspiciatis animi vero fuga explicabo cupiditate molestiae eius. Molestiae tempora, ab explicabo quisquam quia eius dolorem odio aperiam blanditiis ipsum soluta totam aut consectetur tempore necessitatibus ex quaerat nobis voluptas. Eveniet praesentium harum, rem dolor placeat vitae fugit.', hashtags: ['tag6', 'tag7', 'tag8'], category: 'Discussion' },
-],
+        { id: 1, text: 'This is a comment', hashtags: ['tag2', 'tag1'], category: 'General', date: '2022-01-01' },
+        { id: 2, text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate impedit eveniet deserunt, quam atque nisi deleniti esse perferendis commodi voluptas eaque autem quidem quibusdam, corrupti, ratione facilis fugiat molestias distinctio dolores aliquam. Asperiores maiores eligendi quas ducimus quam mollitia ad eius nisi. Repellat hic officiis ea exercitationem natus vero quos!', hashtags: ['tag3', 'tag4'], category: 'Feedback', date: '2022-01-05' },
+        { id: 3, text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur, pariatur! Repudiandae tenetur ea maxime voluptatem saepe commodi adipisci dignissimos molestias?', hashtags: ['tag11', 'tag4'], category: 'Suggestion', date: '2022-01-10' },
+        { id: 4, text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ', hashtags: ['tag5'], category: 'Question', date: '2022-01-12' },
+        { id: 5, text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus, reprehenderit cumque?', hashtags: ['tag6'], category: 'Idea', date: '2022-01-15' },
+        { id: 6, text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos quas, fuga neque voluptas et, a quam impedit obcaecati maiores repellat facere amet dolore tempora ad vel iusto fugiat temporibus laborum dicta praesentium ducimus corrupti reprehenderit animi voluptate? Optio ex quia accusamus labore eveniet nesciunt porro doloremque consequuntur sapiente, sint ducimus perspiciatis. Aspernatur vitae optio alias velit ea, atque corporis suscipit aliquam doloribus! Quos quae sit vel accusamus aliquam quam totam, magnam omnis id facere, dolorum atque asperiores nulla unde praesentium vitae repellat minus, voluptas mollitia ipsa? Quisquam officiis hic sint, corrupti nihil amet consequuntur, illum incidunt earum aperiam quibusdam modi rerum tenetur ullam alias aut voluptatibus ea, in exercitationem deserunt consectetur laboriosam, nihil iure error dolor fuga reiciendis enim! Ipsam, necessitatibus labore autem unde, obcaecati esse corrupti debitis exercitationem enim aspernatur aliquid. Sed fugiat officia expedita assumenda nisi neque quis eligendi repellat, architecto praesentium placeat eos eveniet! Quaerat temporibus sapiente aliquid distinctio ducimus sunt officiis harum, alias mollitia aliquam perspiciatis animi vero fuga explicabo cupiditate molestiae eius. Molestiae tempora, ab explicabo quisquam quia eius dolorem odio aperiam blanditiis ipsum soluta totam aut consectetur tempore necessitatibus ex quaerat nobis voluptas. Eveniet praesentium harum, rem dolor placeat vitae fugit.', hashtags: ['tag6', 'tag7', 'tag8'], category: 'Discussion', date: '2022-01-20' },
+      ],
+      dateRange: {
+        from: null,
+        to: null
+      },
       hashtags: {}, // Global hashtag list
       selectedFilter: [],
       filteredComments: [],
@@ -138,11 +156,13 @@ export default {
   methods: {
     addComment() {
       if (this.newCommentText.trim() && this.newCommentCategory) {
+        const currentDate = new Date().toISOString().split('T')[0];
         this.comments.push({
           id: this.comments.length + 1,
           text: this.newCommentText.trim(),
           hashtags: [],
           category: this.newCommentCategory,
+          date: currentDate,
         })
 
         // Clear input for that specific comment
@@ -211,17 +231,30 @@ export default {
     },
 
     updateFilteredCommentsByCategory() {
-    if (!this.selectedCategory) {
-      this.filteredComments = this.comments;
-    } else {
-      var z = this.selectedCategory;
-      debugger;
-      this.comments.forEach(comment => { console.log(comment.id); console.log(comment.category); });
-      this.filteredComments = this.comments.filter(comment => {
-        return comment.category === this.selectedCategory.value;
-      });
-    }
-  },
+      if (!this.selectedCategory) {
+        this.filteredComments = this.comments;
+      } else {
+        var z = this.selectedCategory;
+        debugger;
+        this.comments.forEach(comment => { console.log(comment.id); console.log(comment.category); });
+        this.filteredComments = this.comments.filter(comment => {
+          return comment.category === this.selectedCategory.value;
+        });
+      }
+    },
+
+    updateFilteredCommentsByDate() {
+      if (this.dateRange?.from && this.dateRange?.to) {
+        this.filteredComments = this.comments.filter(comment => {
+          //console.log(comment.date + '    ' + fromDate + '    ' + toDate);
+
+          return comment.date >= this.dateRange.from && comment.date <= this.dateRange.to;
+        });
+      } else {
+        this.filteredComments = this.comments;
+      }
+    },
+
 
     showTagModal(commentId) {
       this.showTagModalVisible[commentId] = true
